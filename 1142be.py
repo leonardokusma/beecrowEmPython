f519401243781769b7a0c15496e30e69af1a25bf
leonardo kusma <leonardokusma48@gmail.com>
2025-04-13 21:20:42 -0300

    Acabou!!!!

---
+++ 1142be.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+num = 1
+
+for i in range(n):
+    print("%d %d %d PUM" %(num,num+1,num+2))
+    num+= 4
---
+++ 1143be.py
@@ -0,0 +1,4 @@
+n = int(input())
+
+for i in range(1,n+1):
+    print("%d %d %d" %(i, i*i,i*i*i))
